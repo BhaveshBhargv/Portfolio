0dde15086b967635629056fc3364b9324132076a
bhaveshbhargv <@bhavesh.bhargv@gamil.com>
2019-08-30 20:49:36 +0530

    Updates

---
+++ precache-manifest.d2249388cf8e94279929107c4abca0aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be92a6ee075318777af59b1326dbbd6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "18bc7c4319dead9b358e",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0fbabeb93e68157f6ae2",
+    "url": "/Portfolio/static/js/2.a541aa0c.chunk.js"
+  },
+  {
+    "revision": "18bc7c4319dead9b358e",
+    "url": "/Portfolio/static/js/main.69ac84d7.chunk.js"
+  },
+  {
+    "revision": "477435e083081aef76f5",
+    "url": "/Portfolio/static/js/runtime~main.28447a05.js"
+  }
+]);
